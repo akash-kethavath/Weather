da710658d2b1093b488e89f538fe112451f5047f
akash-kethavath <153816103+akash-kethavath@users.noreply.github.com>
2024-10-02 01:42:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,42 +1,55 @@
 let weather = {
-    "apikey": WEATHER_API_KEY,
-    fetchWeather : function (city) {
-        fetch("https://api.openweathermap.org/data/2.5/weather?q=" 
-        + city 
-        + "&units=metric&appid=" 
-        + this.apikey)
-        .then((Response) => Response.json())
-        .then((data) => this.displayWeather(data));
+    fetchWeather: function (city) {
+        // First, we need to convert the city name to latitude and longitude using a geocoding service
+        fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${city}`)
+        .then((response) => response.json())
+        .then((geoData) => {
+            if (geoData.results && geoData.results.length > 0) {
+                const { latitude, longitude, name, country } = geoData.results[0];
+                this.getWeatherData(latitude, longitude, name, country);
+            } else {
+                console.error("City not found");
+            }
+        })
+        .catch((error) => console.error("Error fetching geolocation data:", error));
     },
 
-    displayWeather: function(data) {
-        const { name } = data;
-        const { icon, description } = data.weather[0];
-        const { temp, humidity } = data.main;
-        const { speed } = data.wind;
-        document.querySelector(".city").innerText = "Weather in " + name;
-        document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
-        document.querySelector(".description").innerText = description;
-        document.querySelector(".temp").innerText = temp + "°C";
-        document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
-        document.querySelector(".wind").innerText = "Wind speed: " + speed + " km/h";
+    getWeatherData: function (latitude, longitude, cityName, country) {
+        fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current_weather=true&temperature_unit=celsius&windspeed_unit=kmh`)
+        .then((response) => response.json())
+        .then((data) => this.displayWeather(data, cityName, country))
+        .catch((error) => console.error("Error fetching weather data:", error));
+    },
+
+    displayWeather: function (data, cityName, country) {
+        const { temperature, windspeed } = data.current_weather;
+
+        // Since Open-Meteo API doesn't provide icons or descriptions, you can use a default description
+        document.querySelector(".city").innerText = `Weather in ${cityName}, ${country}`;
+        document.querySelector(".icon").src = "https://www.example.com/default-icon.png"; // You can choose a default weather icon here
+        document.querySelector(".description").innerText = "Current weather";
+        document.querySelector(".temp").innerText = temperature + "°C";
+        document.querySelector(".humidity").innerText = ""; // Open-Meteo doesn't provide humidity
+        document.querySelector(".wind").innerText = "Wind speed: " + windspeed + " km/h";
         document.querySelector(".weather").classList.remove("loading");
-        document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?" + name + "')"
+        document.body.style.backgroundImage = `url('https://source.unsplash.com/1600x900/?${cityName}')`;
     },
 
-    search : function() {
+    search: function () {
         this.fetchWeather(document.querySelector(".searchbar").value);
     }
 };
 
-document.querySelector(".search button").addEventListener("click", function() {
+document.querySelector(".search button").addEventListener("click", function () {
     weather.search();
 });
 
-document.querySelector(".searchbar").addEventListener("keyup", function(event){
-    if(event.key == "Enter"){
+document.querySelector(".searchbar").addEventListener("keyup", function (event) {
+    if (event.key == "Enter") {
         weather.search();
     }
 });
 
-weather.fetchWeather("durgapur");
+// Default city weather when the page loads
+weather.fetchWeather("Durgapur");
+
